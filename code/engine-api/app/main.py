bbbb3ed7d28c26390c30167d13f84763e3f17741
Coleman Lyski <coleman.lyski@gmail.com>
2021-01-25 11:31:13 -0500

    new Flask layout

--- code/engine-api/app/main.py
+++ code/engine-api/app/main.py
@@ -1,4 +1,8 @@
 from app import app
 
 if __name__ == '__main__':
+    # import test routes
+    from routes.test_routes import test_routes as test_routes_blueprint
+    app.register_blueprint(test_routes_blueprint)
+
     app.run()
