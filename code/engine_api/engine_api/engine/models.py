a0e417f6bfa26b3b3afe0309ecdfffbc6d1f4fc4
Coleman Lyski <coleman.lyski@gmail.com>
2021-03-22 13:48:33 -0400

    added new endpoints and models

--- code/engine_api/engine_api/engine/models.py
+++ code/engine_api/engine_api/engine/models.py
@@ -30,11 +30,14 @@ class AnalysisRequest(PkModel):
     """A Sentiment Analysis request coming from Node.js"""
 
     __tablename__ = "analysis_request"
-    keywords = Column(db.Text, nullable=False) # TODO: using 'Text' type, once we implement a limit it can be switched to 'String()' type
+    keywords = Column(db.Text,
+                      nullable=False)  # TODO: using 'Text' type, once we implement a limit it can be switched to 'String()' type
     opened_at = Column(db.DateTime, nullable=False, default=dt.datetime.utcnow)
     status = Column(db.Enum(StatusEnum), nullable=False, default=StatusEnum.CREATED)
     text_twitter = relationship("TextTwitter", back_populates="analysis_request")
     text_reddit = relationship("TextReddit", back_populates="analysis_request")
+    text_tumblr = relationship("TextTumblr", back_populates="analysis_request")
+    analysis_results = relationship("AnalysisResults", uselist=False, back_populates="analysis_request")
 
     def __init__(self, keywords, **kwargs):
         """Create instance."""
@@ -62,10 +65,6 @@ class AnalysisRequest(PkModel):
 
 class TextTwitter(PkModel):
     """Table that stores all Tweets related to an AnalysisRequest"""
-    """
-    posted_at: DateTime
-    text: String
-    """
 
     __tablename__ = "text_twitter"
     analysis_request_id = Column(db.Integer, db.ForeignKey("analysis_request.id"), nullable=False)
@@ -100,10 +99,6 @@ class TextTwitter(PkModel):
 
 class TextReddit(PkModel):
     """Table that stores all Reddit posts/comments related to an AnalysisRequest"""
-    """
-    posted_at: DateTime
-    text: String
-    """
 
     __tablename__ = "text_reddit"
     analysis_request_id = Column(db.Integer, db.ForeignKey("analysis_request.id"), nullable=False)
@@ -134,3 +129,76 @@ class TextReddit(PkModel):
     def __repr__(self):
         """Represent instance as a unique string."""
         return f"<TextReddit({self.id!r})>"
+
+
+class TextTumblr(PkModel):
+    """Table that stores all Tumblr posts/comments related to an AnalysisRequest"""
+
+    __tablename__ = "text_tumblr"
+    analysis_request_id = Column(db.Integer, db.ForeignKey("analysis_request.id"), nullable=False)
+    analysis_request = relationship("AnalysisRequest", back_populates="text_tumblr")
+    created_at = Column(db.DateTime, nullable=False)
+    text = Column(db.Text, nullable=False)
+    is_analyzed = Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, analysis_request_id, created_at, text, **kwargs):
+        super().__init__(
+            analysis_request_id=analysis_request_id,
+            created_at=created_at,
+            text=text,
+            **kwargs
+        )
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'analysis_request_id': self.analysis_request_id,
+            'created_at': self.created_at.isoformat(),
+            'text': self.text,
+            'is_analyzed': self.is_analyzed
+        }
+
+    def __repr__(self):
+        """Represent instance as a unique string."""
+        return f"<TextTumblr({self.id!r})>"
+
+
+class AnalysisResults(PkModel):
+    """Table that stores the Analysis Results for an Analysis Request"""
+
+    __tablename__ = "analysis_results"
+    analysis_request_id = Column(db.Integer, db.ForeignKey("analysis_request.id"), nullable=False)
+    analysis_request = relationship("AnalysisRequest", back_populates="analysis_results")
+    twitter_analysis_complete = Column(db.Boolean, nullable=False, default=False)
+    reddit_analysis_complete = Column(db.Boolean, nullable=False, default=False)
+    tumblr_analysis_complete = Column(db.Boolean, nullable=False, default=False)
+
+    def __init__(self, analysis_request_id, **kwargs):
+        super().__init__(
+            analysis_request_id=analysis_request_id,
+            **kwargs
+        )
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'analysis_request_id': self.analysis_request_id,
+            'twitter_analysis_complete': self.twitter_analysis_complete,
+            'reddit_analysis_complete': self.reddit_analysis_complete,
+            'tumblr_analysis_complete': self.tumblr_analysis_complete,
+        }
+
+    @property
+    def analysis_complete(self):
+        """True if Twitter, Reddit, and Tumblr analysis is complete, else false"""
+        return self.twitter_analysis_complete and \
+               self.reddit_analysis_complete and \
+               self.tumblr_analysis_complete
+
+    def __repr__(self):
+        """Represent instance as a unique string"""
+        return f"<AnalysisResults({self.id!r})>"
